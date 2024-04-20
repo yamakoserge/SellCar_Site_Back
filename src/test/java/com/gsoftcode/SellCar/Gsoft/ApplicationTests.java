9241796d295321582a47fc71b5ee333d847ce932
Serges YAMAKO <yamakoserges@gmail.com>
2024-04-20 16:38:16 +0100

    beginner project to sell car website back

---
+++ src/test/java/com/gsoftcode/SellCar/Gsoft/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsoftcode.SellCar.Gsoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
