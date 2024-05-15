505d44b5dcd85ca1ee43513f9e3c6015ef20bd1a
Serges YAMAKO <yamakoserges@gmail.com>
2024-05-15 13:56:03 +0100

    create login/generate JWT Token Api In Spring boot

--- src/main/java/com/gsoftcode/SellCar/Gsoft/controller/AuthController.java
+++ src/main/java/com/gsoftcode/SellCar/Gsoft/controller/AuthController.java
@@ -1,15 +1,28 @@
 package com.gsoftcode.SellCar.Gsoft.controller;
 
+import com.gsoftcode.SellCar.Gsoft.dtos.AuthenticationRequest;
+import com.gsoftcode.SellCar.Gsoft.dtos.AuthenticationResponse;
 import com.gsoftcode.SellCar.Gsoft.dtos.SignupRequest;
 import com.gsoftcode.SellCar.Gsoft.dtos.UserDTO;
+import com.gsoftcode.SellCar.Gsoft.entities.User;
+import com.gsoftcode.SellCar.Gsoft.repository.UserRepository;
 import com.gsoftcode.SellCar.Gsoft.services.auth.AuthService;
+import com.gsoftcode.SellCar.Gsoft.services.jwt.UserService;
+import com.gsoftcode.SellCar.Gsoft.utils.JWTUtil;
+import io.jsonwebtoken.Jwt;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,6 +30,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthService authService;
+
+    private final JWTUtil jwtUtil;
+
+    private final UserService userService;
+
+    private final UserRepository userRepository;
+
+    private final AuthenticationManager authenticationManager;
 
     @PostMapping("/signup")
     public ResponseEntity<?> signup(@RequestBody SignupRequest signupRequest){
@@ -27,4 +48,30 @@ public class AuthController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         return ResponseEntity.status(HttpStatus.CREATED).body(userDTO);
     }
-}
+
+    PostMapping("/login")
+    public AuthenticationResponse login(@RequestBody AuthenticationRequest authenticationRequest){
+        try{
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            authenticationRequest.getEmail(),
+                            authenticationRequest.getPassword()));
+
+
+    }catch (BadCredentialsException e){
+            throw new BadCredentialsException("Login ou Mot de passe sont incorrectes")
+        }
+
+final UserDetails userDetails = userService.userDetailService().loadUserByUsername(authenticationRequest.getEmail());
+        Optional<User> optionalUser = userDetails.findFirstByEmail(authenticationRequest.getEmail());
+        final String jwt = jwtUtil.generateToken(userDetails);
+        AuthenticationResponse response = new AuthenticationResponse();
+        if (optionalUser.isPresent()){
+            response.setJwt(jwt);
+            response.setUserRole(optionalUser.get().getUserRole());
+            response.setUserId(optionalUser.get().getId());
+        }
+        return response;
+
+    }
+    }
